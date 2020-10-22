3511eec2cd7290badc795bda6c963e4e2a7b75b1
Liam King <51165020+liam-king-99@users.noreply.github.com>
2020-10-22 14:49:08 -0400

    Updated the data model

--- dataModel.js
+++ dataModel.js
@@ -1,30 +1,43 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/test", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost/test", {useNewUrlParser:true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function(){
     // connected
     const assignmentSchema = new mongoose.Schema({
-        assignmentData: '',               //key to database
-        assignmentTitle: '',              //should this be a variable passed in
-        assignmentType: '',               //should this be null
-        dueDate: Date,                      //get from user input form
-        frequency: '',                    //get from user input form
-        patientName: '',                  //get from user input form
-        done: false,                        //starts false as default
-        specialInstructions: ''           //get from user input form
+        exerciseList: Array,        // Array of IDs or objects?
+        dateAssigned: Date,
+        patientName: String,        // Do we need name and ID?
+        patientID: String,
+        therapistName: String,      // Do we need name and ID?
+        therapistID: String,
+        assignmentProgress: Number,
+        visitNumber: Number         // Count the number of visits so far
+    });
+
+    const exerciseSchema = new mongoose.Schema({
+        exerciseData: String,      // key to exercises in cloud
+        exerciseTitle: String,     // this and the below would be passed in as a variable from form, except for "done"
+        exerciseType: String,
+        dueDate: Date,
+        frequency: String,
+        patientName: String,
+        patientID: String,
+        progress: Number,
+        specialInstructions: String
     });
 
     const Grounding = mongoose.model("Grounding", assignmentSchema)
     const flashback = new Grounding({
-        assignmentData: "flashbackKey",
-        assignmentTitle: "Flashback Grounding",     // this and the below would be passed in as a variable from form, except for "done"
-        assignmentType: "Grounding",
-        dueDate: new Date(),
+        exerciseData: "flashbackKey",
+        exerciseTitle: "Flashback Grounding",     // this and the below would be passed in as a variable from form, except for "done"
+        exerciseType: "Grounding",
+        dueDate: new Date(Date.now() + 12096e5),    // Right now plus two weeks
         frequency: "Weekly",
         patientName: "Liam",
-        done: false,
+        patientID: "uniqueIdentifier",
+        progress: 0,
         specialInstructions: "Please let me know if you need any help!"
     });
 
@@ -32,5 +45,3 @@ db.once("open", function(){
     });
     
 });
-
-
