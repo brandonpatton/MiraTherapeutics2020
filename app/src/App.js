68648415991e454f3593e3b42c85e7c610274af8
Pratim Patel <ppate78@stevens.edu>
2020-12-03 16:40:50 -0500

    made changes to login page

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import logo from './Mira.png';
+import logo from './Mira.jpg';
+import banner from './MiraBanner.png';
 import './App.css';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { MDBCard, MDBCardBody, MDBContainer,MDBCardTitle,MDBCardText } from "mdbreact";
 
 class App extends Component {
   constructor(props) {
@@ -20,20 +22,57 @@ class App extends Component {
   componentDidMount() {
       this.callAPI();
   }
-
-  render() {
-    return (
+  render(){
+    return(
       <div className="App">
+        <header className = "Mira-site-header">
+            <img src={banner} className="Mira-banner" alt="banner"/>
+          </header>
         <div className = "App-logo-container">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
+        <body>
         <div className = "App-background-container">
           <div className = "bg"></div>
         </div> 
+        </body>
+        <div className = "Login-container">
+          <form>
+            <input className = "Username" type="text" placeholder="Username" id="unamee" name="username"></input>
+           <input className = "Password" type="text" placeholder="Password"id="pword" name="password"></input>
+         </form>
+        </div>
+        <div>
+          
+        <MDBContainer className = "Card">
+          <MDBCard className="card-body">
+            <MDBCardTitle>Miratx Articles</MDBCardTitle>
+            <MDBCardText>
+              Articles from Miratx
+            </MDBCardText>
+            <div className="flex-row">
+            </div>
+          </MDBCard>
+          
+  </MDBContainer>
+  </div>
+  <MDBContainer className = "Card-2">
+          <MDBCard className="card-body-2">
+            <MDBCardTitle>Miratx Articles</MDBCardTitle>
+            <MDBCardText>
+              Articles from Miratx
+            </MDBCardText>
+            <div className="flex-row-2">
+            </div>
+          </MDBCard>
+          
+  </MDBContainer>
         <p className="App-intro">{this.state.apiResponse}</p>
       </div>
-    );
+    )
   }
 }
+
+
 
 export default App;
