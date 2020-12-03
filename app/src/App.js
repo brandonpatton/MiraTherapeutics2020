6843a20bdf994df139fa0878984a4ff4d3f6a101
brandonpatton <brandonpattonpf@gmail.com>
2020-12-02 19:07:30 -0500

    Added login page prototype

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './Mira.png';
 import './App.css';
 
+
+<div className = "body"></div>
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className = "App-logo-container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className = "App-background-container">
+        <div className = "bg"></div>
+      </div>      
     </div>
   );
 }
