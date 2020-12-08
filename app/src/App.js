cb28e557c4fce96dd6638b0713802dcd0c64423f
Pratim Patel <ppate78@stevens.edu>
2020-12-08 16:50:51 -0500

    frontend refactoring and additions

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import logo from './Mira.jpg';
 import banner from './MiraBanner.png';
 import bubble from './bubbles.png';
@@ -7,6 +8,7 @@ import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MDBCard, MDBCardBody, MDBContainer,MDBCardTitle,MDBCardText } from "mdbreact";
+import {Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -23,61 +25,76 @@ class App extends Component {
   componentDidMount() {
       this.callAPI();
   }
+
   render(){
     return(
       <div className="App">
+
         <header className = "Mira-site-header">
             <img src={banner} className="Mira-banner" alt="banner"/>
-          </header>
+        </header>
+
         <div className = "App-logo-container">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
+
         <body>
-        <div className = "App-background-container">
-          <div className = "bg"></div>
-        </div> 
+          <div className = "App-background-container">
+            <div className = "bg"></div>
+          </div> 
         </body>
-        <div className = "bubbles">
-        <img src={bubble} className="bubble1" alt="bubble" />
+
+
+        <div className = "Username-bubble-image">
+          <img src={bubble} className="bubble1" alt="bubble" />
         </div>
-        <div className = "bubbles2">
-        <img src={bubble} className="bubble2" alt="bubble" />
+
+        <div className = "Password-bubble-image">
+          <img src={bubble} className="bubble2" alt="bubble" />
         </div>
+
         <div className = "Login-container">
           <form>
             <input className = "Username" type="text" placeholder="Username" id="unamee" name="username"></input>
-           <input className = "Password" type="text" placeholder="Password"id="pword" name="password"></input>
+            <input className = "Password" type="text" placeholder="Password"id="pword" name="password"></input>
          </form>
         </div>
-        <div>
-        <button  className = "Button" type="button">Log In</button>
+
+
+        <Link to = "/page2"><Button  className = "Button" onclick="window.location.href='/page2'"type="button">Log In</Button></Link>
+
         <div className = "New-user-container">
-        <a href = "www.google.com" className = "user">New User?</a>
-        </div>
+          <a href = "www.google.com" className = "user">New User?</a>
+        </div>             
+        
         <div className = "Forgot-password-container">
-        <a href = "www.google.com" className= "pword">Forgot Password?</a>
+          <a href = "www.google.com" className= "pword">Forgot Password?</a>
         </div>
-          
+
+        <div>  
         <MDBContainer className = "Card">
           <MDBCard className="card-body">
-            <MDBCardTitle>Miratx Articles</MDBCardTitle>
+            <MDBCardTitle className = "Card1-title">Miratx Articles</MDBCardTitle>
             <MDBCardText>
               Articles from Miratx
             </MDBCardText>
-          </MDBCard>
-          
-  </MDBContainer>
-  </div>
-  <MDBContainer className = "Card-2">
+          </MDBCard>          
+        </MDBContainer>
+        </div>
+
+        <div>
+        <MDBContainer className = "Card-2">
           <MDBCard className="card-body-2">
-            <MDBCardTitle>Miratx Articles</MDBCardTitle>
+            <MDBCardTitle className = "Card2-title">Miratx Articles</MDBCardTitle>
             <MDBCardText>
               Articles from Miratx
             </MDBCardText>
-          </MDBCard>
-          
-  </MDBContainer>
+          </MDBCard>          
+        </MDBContainer>
+        </div>
+
         <p className="App-intro">{this.state.apiResponse}</p>
+
       </div>
     )
   }
--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Page1 from "./App";
+import Page2 from "./PracticeView";
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Switch>
+   <Route exact path="/" component={Page1} />
+   <Route path="/page2" component={Page2} />
+ </Switch>
+ </BrowserRouter>,
+ rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
