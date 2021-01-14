c897716073d279426a6a2554eb5e7d87941dbe29
brandonpatton <brandonpattonpf@gmail.com>
2021-01-14 17:28:13 -0500

    ClientView page edits

--- app/src/index.js
+++ app/src/index.js
@@ -7,7 +7,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ClientPage from "./views/ClientPage";
 import Page1 from "./App";
 import PracticeView from "./views/PracticeView";
-import ExerciseForm from "./views/ExerciseForm"
+import ExerciseForm from "./views/ExerciseForm";
+import ClientView from "./views/ClientView";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
@@ -15,6 +16,7 @@ ReactDOM.render(
    <Route exact path="/" component={Page1} />
    <Route path="/PracticeView" component={PracticeView} />
    <Route path="/ExerciseForm" component={ExerciseForm} />
+   <Route path="/ClientView" component={ClientView} />
    <Route path="/ClientPage/1" component={ClientPage} />
  </Switch>
  </BrowserRouter>,
