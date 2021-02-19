f557b7d935b81425a9403cfbefa1c2dbbe4d3299
Liam <liamking84@gmail.com>
2021-02-19 02:14:42 -0500

    Complete assignment button

--- app/src/views/ClientView.js
+++ app/src/views/ClientView.js
@@ -9,19 +9,24 @@ import '../css/ClientView.css';
 import { MDBCard, MDBCardTitle } from "mdbreact";
 import {Redirect,Switch} from 'react-router-dom';
 import logo from '../Mira.jpg';
-import {Row, Col, Container, Image, Card, Button} from 'react-bootstrap'
+import {Row, Col, Container, Image, Card, /*Button*/} from 'react-bootstrap'
 import Form from 'react-bootstrap/Form';
 import picture from '../Bonelli-RECT.jpg';
 import { useHistory } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   CircularProgressbar,
-  CircularProgressbarWithChildren,
   buildStyles
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { LinearProgress } from '@material-ui/core';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class ClientView extends Component {
   
@@ -35,52 +40,18 @@ class ClientView extends Component {
           assignments: [
               {
                   due: '',
-                  status: '',
+                  // status of assignments numbers: 0 - New, 1 - Ongoing, 2 - Completed
+                  status: 0,
                   exercises: [
                   ]
               }
           ]
         },
-        selectedAssignment: 0
+        selectedAssignment: {},
+        assignmentCompletionDialogOpen: false
 
       }
-      this.bubbleInfo = [
-        {
-          assignmentProgress: 100,
-          due: "11/20",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 50,
-          due: "11/27",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 100,
-          due: "12/04",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 100,
-          due: "12/12",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 100,
-          due: "12/19",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 100,
-          due: "12/26",
-          status: "Completed"
-        },
-        {
-          assignmentProgress: 100,
-          due: "1/02",
-          status: "Ongoing"
-        }
-      ]
+      this.completionStatusWords = ["New", "Ongoing", "Completed"]
       this.clientInfo = [
         {
           picture: picture,
@@ -91,6 +62,8 @@ class ClientView extends Component {
       ]
 
       changeVisibleAssignment = changeVisibleAssignment.bind(this);
+      completeAssignmentButton = completeAssignmentButton.bind(this)
+      completeAssignment = completeAssignment.bind(this)
 
     }
 
@@ -98,8 +71,12 @@ class ClientView extends Component {
       fetch("http://localhost:3080/assignments/patient/PjohnDoe1")
           .then(res => res.json())
           .then(data => {
+            // sort the assignments based on the number of the visit during which they were assigned
             data.sort((a, b) => a.visitNumber - b.visitNumber)
-            this.setState({patient: {assignments: data}})
+            // update the state with the assignments in the right order
+            this.setState(() => ({patient: {assignments: data}}))
+            // make the assignment that's visible to the therapist the most recent one
+            this.setState((state) => ({selectedAssignment: state.patient.assignments[state.patient.assignments.length - 1]}))
           });
 
     }
@@ -144,22 +121,25 @@ class ClientView extends Component {
                                   <Row>
                                       <Col>
                                         <div className = "Assignment-completion-status-text-container">
-                                          <MDBCardTitle className="Assignment-completion-status-text">{this.state.patient.assignments[this.state.selectedAssignment].status}</MDBCardTitle> {/*Get this from patient data*/}
+                                          <MDBCardTitle className="Assignment-completion-status-text">{this.completionStatusWords[this.state.selectedAssignment.status]}</MDBCardTitle> {/*Get this from patient data*/}
                                         </div>
                                       </Col>
                                       <Col>
                                         <div className = "Assignment-due-date-container">
-                                          <MDBCardTitle className="Assignment-due-date-text">Due by: <u>{this.state.patient.assignments[this.state.selectedAssignment].due}</u> </MDBCardTitle> {/*Get this from patient data*/}
+                                          <MDBCardTitle className="Assignment-due-date-text">Due by: <u>{this.state.selectedAssignment.due}</u> </MDBCardTitle> {/*Get this from patient data*/}
                                         </div>
                                       </Col>
-                                       
+                                      <Col>
+                                        {completeAssignmentButton()}
+                                      </Col>
                                   </Row>
                                   <Row>
                                       <Col>
                                         <div className = "Assignment-progress-container">
                                           <MDBCardTitle className="Assignment-completion-title-text">Assignment Completion</MDBCardTitle>
                                           <div className="Exercise-data-container">
-                                            {getExercises(this.state.patient.assignments[this.state.selectedAssignment].exerciseList)}
+                                            {/*getExercises(this.state.patient.assignments[this.state.selectedAssignment].exerciseList)  until selectedAssignment being an object works*/}
+                                            {getExercises(this.state.selectedAssignment.exerciseList)}
                                           </div>
                                         </div>
                                       </Col>
@@ -176,20 +156,99 @@ class ClientView extends Component {
     }
   }
 
-function changeVisibleAssignment(assignmentNumber) {
+function completeAssignmentButton() {
+
+  const handleClickOpen = () => {
+    this.setState(() => ({
+      assignmentCompletionDialogOpen: true
+    }))
+  };
+  
+  const handleClose = () => {
+    this.setState(() => ({
+      assignmentCompletionDialogOpen: false
+    }))
+  };
+
+
+  return (<div className = "Complete-assignment-button-div">
+  <Button className = "clickMe" variant="outlined" color="primary" onClick={handleClickOpen} disabled = {this.state.selectedAssignment.status == 2}>
+        {this.state.selectedAssignment.status == 0 ? "Create Assignment" : "Complete Assignment"}
+      </Button>
+      <Dialog
+        open = {this.state.assignmentCompletionDialogOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{this.state.selectedAssignment.status == 1 ? "Complete Assignment?" : "Create Assignment?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {this.state.selectedAssignment.status == 1 ? "Are you sure you want to complete the selected assignment?" : "Would you like to create a new assignment?"}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.state.selectedAssignment.status == 1 ? completeAssignment : (() => {alert("You want to make a new assignment"); this.setState(() => ({assignmentCompletionDialogOpen: false}))})} color="primary">
+            Yes
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </div>)
+}
+
+function completeAssignment() {
+  // Assignments are sorted in descending order of visitNumber (whatever nth visit during which they were assigned)
+  // change status of selected assignment to "Complete"
+  let targetVisitNumber = this.state.selectedAssignment.visitNumber
+  let data = this.state.patient.assignments
+  for (let assignment of data) {
+    if (assignment.visitNumber == targetVisitNumber) {
+      assignment.status = 2
+      break
+    }
+  }
+
+  // make a new barebones assignment so that a new bubbble can be made. Just exists with required fields
+  let newAssignmentForNewBubble = {
+    visitNumber: data.length + 1,
+    due: "N/A",
+    assignmentProgress: 0,
+    status: 0
+  }
+  data.push(newAssignmentForNewBubble)
   this.setState(() => ({
-    selectedAssignment: assignmentNumber
+    patient: {assignments: data},
+    selectedAssignment: data[data.length - 1],
+    assignmentCompletionDialogOpen: false
   }))
 }
 
-let getBubbleInfo = (bubbleInfo) => {
+function changeVisibleAssignment(visitNumber) {
+  // change the assignment being shown to a new one based on the visitNumber provided by the assignment bubble
+  let correspondingAssignmentByVisitNumber;
+  for (let assignment of this.state.patient.assignments) {
+    if (assignment.visitNumber == visitNumber) {
+      correspondingAssignmentByVisitNumber = assignment
+      break
+    }
+  }
+  this.setState(() => ({
+    selectedAssignment: correspondingAssignmentByVisitNumber
+  }))
+}
+
+let getBubbleInfo = (assignmentsList) => {
   const ongoingBubbleColor = '#00b5d9'
   const completedBubbleColor = '#20315f'
   const completedBubblePathColor = '#FFFFFF'
     let progressBubbleComponents = []
-    // 7 Assignments visible at a time
-    for (let assignmentIndex = 6; assignmentIndex >= 0; assignmentIndex--) {
-      if (assignmentIndex >= bubbleInfo.length) {
+    // 7 Assignments on the row at a time
+    for (let assignmentIndex = 0; assignmentIndex < 7; assignmentIndex++) {
+      // Make an invisible assignment bubble so the spacing still works out
+      if (assignmentsList.length <= assignmentIndex) {
         progressBubbleComponents.unshift(
           <div className = "Progress-bubble-column-hidden">
             <CircularProgressbar  className = "Progress-bubbles"
@@ -198,16 +257,18 @@ let getBubbleInfo = (bubbleInfo) => {
           </div>
         )
       } else {
+        let targetIndex = assignmentsList.length - assignmentIndex - 1
+        let targetAssignment = assignmentsList[targetIndex]
         progressBubbleComponents.unshift(
-          <div className = "Progress-bubble-column" onClick = {() => changeVisibleAssignment(assignmentIndex)}>
-            <CircularProgressbar  className = "Progress-bubbles" value={bubbleInfo[assignmentIndex].assignmentProgress}
-            text={`${bubbleInfo[assignmentIndex].due}`}
+          <div className = "Progress-bubble-column" onClick = {() => changeVisibleAssignment(targetAssignment.visitNumber)}>
+            <CircularProgressbar  className = "Progress-bubbles" value={targetAssignment.assignmentProgress}
+            text={`${targetAssignment.due}`}
             background
             backgroundPadding={6}
             styles={buildStyles({
-              backgroundColor: `${bubbleInfo[assignmentIndex].status == "Ongoing" ? ongoingBubbleColor : completedBubbleColor}`,
+              backgroundColor: `${targetAssignment.status == 2 ? completedBubbleColor : ongoingBubbleColor}`,
               textColor: "#fff",
-              pathColor: `${bubbleInfo[assignmentIndex].status == "Ongoing" ? '#000000' : completedBubblePathColor}`,
+              pathColor: `${targetAssignment.status == 2 ? completedBubblePathColor : '#000000'}`,
               trailColor: "transparent"
             })}/>
           </div>)
